26cba1c45e4a1896f1a4edea1bad3ba62ee90743
“javier.garciadeleani” <javier.garciadeleanizgonzalez@es.ey.com>
2021-07-25 12:19:30 +0200

    feature: search in DB by default

--- normalize_my_places.py
+++ normalize_my_places.py
@@ -49,6 +49,31 @@ def save_fail_info(place_doc, elast, idx='restaurants-gordologo-fail'):
         # save to elastic
         return save_to_elastic(place_doc, elast, idx, place_id)
 
+def search_name(name, gmaps, elast):
+    query = {
+        "query_string": {
+            "default_field": "name",
+            "query": f"\"{name}\""
+            }
+        }
+    # search default index
+    res = elast.search(index='restaurants-gordologo-default', 
+                       body={"query": query})
+    hits = res['hits']['total']['value']
+
+    # search fail index
+    res = elast.get(index='restaurants-gordologo-fail', 
+                    id=name, ignore=404)
+
+    if hits == 1 or res['found']:
+        return {'status': 'EXISTING'}
+    # if multiple hits, we are unsure if the match is right, so we search again
+    else:
+        r = gmaps.find_place(name, 
+                             'textquery', 
+                             fields=['place_id', 'formatted_address', 'name'])
+        return r
+
 def get_places_info_from_names(places_names, gmaps_client, elastic_client):
     """
         Call Google API to get places information.
@@ -56,17 +81,22 @@ def get_places_info_from_names(places_names, gmaps_client, elastic_client):
     for i, place in enumerate(places_names):
         if not i % 10:
             print(f'{i} places processed')
-        r = gmaps_client.find_place(place, 
-                                    'textquery', 
-                                    fields=['place_id', 'formatted_address', 'name'])
+        # search in DB first, then in gmaps API 
+        r = search_name(place, gmaps_client, elastic_client)
+
+        # gmaps response, search for detailed info and save it in DB
         if r['status'] in ['OK', 'ZERO_RESULTS']:
             n_candidates = len(r['candidates'])
             if n_candidates == 1:
                 place_id = r['candidates'][0]['place_id']
                 status = get_save_detailed_info(place_id, gmaps_client, elastic_client)
+            # ambiguous candidates or 0 candidates, save it to fail index
             else:
                 doc = {'name': place, 'candidates': r['candidates']}
-                status = save_fail_info(doc, elastic_client, 'restaurants-gordologo-fail')                
+                status = save_fail_info(doc, elastic_client, 'restaurants-gordologo-fail')
+        # existing in DB
+        elif r['status'] == 'EXISTING':
+            print(f'Place <{place}> already in DB')
         else:
             print(f'Unsuccesful request:\n {r}')
             return False
@@ -80,7 +110,7 @@ def save_places(output_p, places_json):
 
 if __name__ == '__main__':
     # "Quiero ir" file comes from Google Takeout, selecting "Save" category <https://takeout.google.com/settings/takeout?hl=es>
-    input_file = './data/Quiero ir_short.csv'
+    input_file = './data/Quiero ir.csv'
     api_key_file = './api_key.txt'
     output_file = './data/restaurants.json'
 
