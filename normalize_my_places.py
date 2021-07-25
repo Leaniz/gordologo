31f6e72d2d2becfbbbffafe367d82b555a72fcce
“javier.garciadeleani” <javier.garciadeleanizgonzalez@es.ey.com>
2021-07-25 10:42:15 +0200

    feature: use google maps and elasticsearch python clients

--- normalize_my_places.py
+++ normalize_my_places.py
@@ -1,6 +1,7 @@
 import pandas as pd
-import requests
+import googlemaps
 import json
+from elasticsearch import Elasticsearch
 
 def get_api_key(p):
     with open(p, 'r') as f:
@@ -10,44 +11,55 @@ def get_api_key(p):
 def get_places_names(csv_p):
     """get places names from exported URL"""
     df = pd.read_csv(csv_p)
-    return [p.split('/')[5] for p in df.URL.tolist()]
+    return [p for p in df.Titulo.tolist()]
 
-def get_places_info_from_names(places_names, api_key):
+def save_to_elastic(doc, elast, idx, id=None):
+    if id is not None:
+        res = elast.index(index=idx, id=id, body=doc)
+    else:
+        res = elast.index(index=idx, body=doc)
+
+    if res['result'] != 'created':
+        raise Exception(f'Restaurant <{doc}> was not created.\n Result: <{res}>')
+    else:
+        return 200
+
+def get_save_detailed_info(place_id, gmaps, elast, idx='restaurants-gordologo-default'):
+    # check if document already in DB
+    res = elast.get(index=idx, id=place_id, ignore=404)
+    if res['found']:
+        name = res['_source']['name']
+        print(f'Place <{name}> already in DB')
+        return 404
+    else:
+        # get detailed information
+        place_doc = gmaps.place(place_id)
+        # save to elastic
+        return save_to_elastic(place_doc['result'], elast, idx, place_id)
+
+def get_places_info_from_names(places_names, gmaps_client, elastic_client):
     """
         Call Google API to get places information.
     """
-    places_json = {'places': [],
-                    'ambiguous_places': [],
-                    'places_not_found': []
-                    }
-
-    return_fields = ['place_id',
-                    'formatted_address',
-                    'name',
-                    'rating',
-                    'types',
-                    'price_level',
-                    'user_ratings_total']
-
     for i, place in enumerate(places_names):
         if not i % 10:
             print(f'{i} places processed')
-        str_returns_fields = ','.join(return_fields)
-        r = requests.get(f'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input={place}&inputtype=textquery&fields={str_returns_fields}&key={api_key}')
-        if r.status_code == 200:
-            place_data = r.json()
-            n_candidates = len(place_data['candidates']) 
+        r = gmaps_client.find_place(place, 
+                                    'textquery', 
+                                    fields=['place_id', 'formatted_address', 'name'])
+        if r['status'] in ['OK', 'ZERO_RESULTS']:
+            n_candidates = len(r['candidates'])
             if n_candidates == 1:
-                places_json['places'].append({place: place_data['candidates'][0]})
-            elif n_candidates == 0:
-                places_json['places_not_found'].append({place: place_data['candidates']})
+                place_id = r['candidates'][0]['place_id']
+                status = get_save_detailed_info(place_id, gmaps_client, elastic_client)
             else:
-                places_json['ambiguous_places'].append({place: place_data['candidates']})
+                doc = {'name': place, 'candidates': r['candidates']}
+                status = save_to_elastic(doc,elastic_client, 'restaurants-gordologo-fail')                
         else:
-            print(f'Unsuccesful request:\n {r.json()}')
-            return places_json, False
+            print(f'Unsuccesful request:\n {r}')
+            return False
     
-    return places_json, True
+    return True
 
 def save_places(output_p, places_json):
     with open(output_p, 'w', encoding='utf-8') as f:
@@ -56,15 +68,15 @@ def save_places(output_p, places_json):
 
 if __name__ == '__main__':
     # "Quiero ir" file comes from Google Takeout, selecting "Save" category <https://takeout.google.com/settings/takeout?hl=es>
-    input_file = './data/Quiero ir.csv'
+    input_file = './data/Quiero ir_short.csv'
     api_key_file = './api_key.txt'
     output_file = './data/restaurants.json'
 
     api_key = get_api_key(api_key_file)
     places_names = get_places_names(input_file)
 
-    places_json, val = get_places_info_from_names(places_names, api_key)
-    if val:
-        save_places(output_file, places_json)
-    else:
+    gmaps = googlemaps.Client(key=api_key)
+    elast = Elasticsearch('localhost', port=9200)
+    val = get_places_info_from_names(places_names, gmaps, elast)
+    if not val:
         print("Results not saved due to unsuccessful request")
