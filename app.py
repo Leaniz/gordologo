8f3a19aea2e23ff16e603d05b15440d225241319
“javier.garciadeleani” <javier.garciadeleanizgonzalez@es.ey.com>
2021-07-28 17:35:06 +0200

    config: increase token refresh time to 30 min

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
+from datetime import timedelta
 
 from security import authenticate, identity
 from resources.user import UserRegister
@@ -15,4 +16,8 @@ jwt = JWT(app, authenticate, identity) # /auth
 api.add_resource(Restaurant, "/restaurant/<string:place_id>")
 api.add_resource(RestaurantList, "/restaurants")
 api.add_resource(UserRegister, "/register")
+
+# config JWT to expire within half an hour
+app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
+
 app.run(port=5000, debug=True)
